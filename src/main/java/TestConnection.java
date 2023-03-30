0d1bf6d6be64b70c5f67a3c446de6061b4f66476
volRender <litvishko.render@gmail.com>
2023-03-30 15:17:45 +0300

    Create connection parameters and getConnection() method in Util class. A connection check was made in TestConnection class.

---
+++ src/main/java/TestConnection.java
@@ -0,0 +1,8 @@
+import jm.task.core.jdbc.util.Util;
+
+public class TestConnection {
+    public static void main(String[] args) {
+        Util util = new Util();
+        util.getConnection();
+    }
+}
