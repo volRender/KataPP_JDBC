21851259f1f2d10e833dbacb8aace84503a6ea04
volRender <litvishko.render@gmail.com>
2023-03-30 23:55:49 +0300

    Realize Service pattern.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,33 +2,34 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl daoJDBC = new UserDaoJDBCImpl();
+        UserServiceImpl service = new UserServiceImpl();
 
         //create table Users
-        daoJDBC.createUsersTable();
+        service.createUsersTable();
 
         //create 4 users for table data
-        daoJDBC.saveUser("Daniil", "Litvishko", (byte) 23);
-        daoJDBC.saveUser("Vladislav", "Varakin", (byte) 23);
-        daoJDBC.saveUser("Nikita", "Vasiliev", (byte) 22);
-        daoJDBC.saveUser("Lena", "Soroka", (byte) 22);
+        service.saveUser("Daniil", "Litvishko", (byte) 23);
+        service.saveUser("Vladislav", "Varakin", (byte) 23);
+        service.saveUser("Nikita", "Vasiliev", (byte) 22);
+        service.saveUser("Lena", "Soroka", (byte) 22);
 
         //get all users info from the table
-        List<User> userList = daoJDBC.getAllUsers();
+        List<User> userList = service.getAllUsers();
         for (User u :userList) {
             System.out.println(u);
         }
 
         //clean all data from the table
-        daoJDBC.cleanUsersTable();
+        service.cleanUsersTable();
 
         //drop table Users
-        daoJDBC.dropUsersTable();
+        service.dropUsersTable();
     }
 }
